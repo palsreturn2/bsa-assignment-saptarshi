f686dfd33c0243465173a5c147b8dc06d0e09ef2
Saptarshi <saptarshi@tropogo.com>
2022-03-17 16:28:25 +0530

    Added summary document and multiple timeline aerosol layers

--- README.md
+++ README.md
@@ -7,10 +7,16 @@ To view the assignment, please open main.html file.
 ## Getting Started
 
 The following steps can be used to run the code as a container after cloning the respository.
+Please setup docker in your system before you follow the instructions.
+
+The docker installtion procedure can be followed from the following website
+
+https://docs.docker.com/engine/install/
 
 ```bash
 $ docker build -t bsa_assignment_image .
 $ docker run -p 8080:8888 -d --name bsa_assignment_container bsa_assignment_image
+$ docker logs bsa_assignment_container
 ```
 Visit http://127.0.0.1:8080/ in the browser to run the codes.
 
--- main.py
+++ main.py
@@ -6,9 +6,9 @@
 
 import os
 import warnings
-
 warnings.filterwarnings('ignore')
 WORKDIR = os.getcwd()
+
 
 # In[2]:
 
@@ -18,33 +18,34 @@ WORKDIR = os.getcwd()
 from datetime import datetime
 import pandas as pd
 
-# Locations where CPCB air quality sensors are present and are collecting ground truth data
+# Air quality sensor locations of the three ground stations
 
-locations = [
-    'Mandir Marg, Delhi - DPCC',
-    'Chandni Chowk, Delhi - IITM',
-    'Loni, Ghaziabad - UPPCB'
-]
+import geopandas as gpd
+gdf = gpd.read_file(WORKDIR+'/datasets/aq_stations.geojson')
 
-# Lockdown Start date 19th April
+locations = gdf['name']
+print(locations)
+
+#Lockdown Start date 19th April
 
 lockdown_date = datetime.strptime('19-04-2021', '%d-%m-%Y')
-print('Lockdown Date: ', lockdown_date)
+print('Lockdown Date: ',lockdown_date)
 
-aq_sensor_data = pd.read_excel(WORKDIR + '/datasets/aqi_sensor_data.xlsx', header=0)
+aq_sensor_data = pd.read_excel(WORKDIR+'/datasets/aqi_sensor_data.xlsx', header=0)  
 aq_sensor_data['datetime'] = pd.to_datetime(aq_sensor_data['datetime'], format='%A, %d %b %Y %I:%M %p')
 print('Locations Mean AQI')
 for i in range(0, len(locations)):
-    print(locations[i] + "=" + str(aq_sensor_data[aq_sensor_data['name'] == locations[i]]['aqi'].mean()))
+    print(locations[i]+"="+str(aq_sensor_data[aq_sensor_data['name']==locations[i]]['aqi'].mean()))
 print('\nMean AQI after full lockdown period started')
 for i in range(0, len(locations)):
-    print(locations[i] + "=" + str(aq_sensor_data[(aq_sensor_data['name'] == locations[i])
-                                                  & (aq_sensor_data['datetime'] >= lockdown_date)]['aqi'].mean()))
+    print(locations[i]+"="+str(aq_sensor_data[(aq_sensor_data['name']==locations[i]) 
+                                              & (aq_sensor_data['datetime']>=lockdown_date)]['aqi'].mean()))
 
 print('\nMean AQI before full lockdown period started')
 for i in range(0, len(locations)):
-    print(locations[i] + "=" + str(aq_sensor_data[(aq_sensor_data['name'] == locations[i])
-                                                  & (aq_sensor_data['datetime'] < lockdown_date)]['aqi'].mean()))
+    print(locations[i]+"="+str(aq_sensor_data[(aq_sensor_data['name']==locations[i]) 
+                                              & (aq_sensor_data['datetime']<lockdown_date)]['aqi'].mean()))
+
 
 # In[3]:
 
@@ -64,42 +65,35 @@ for i in range(0, len(locations)):
 # In[4]:
 
 
-# Air quality sensor locations of the three ground stations
-
-import geopandas as gpd
-
-gdf = gpd.read_file(WORKDIR + '/datasets/aq_stations.geojson')
-gdf.head()
-
-# In[7]:
-
-
 # Region of interest: Buffer of 0.02 degrees is taken around the ground sensors.
 # Assumption: Near things affect more than distant things.
 
 gdf['geometry'] = gdf['geometry']
 gdf['geometry'] = gdf['geometry'].buffer(0.02)
 
-# In[8]:
+
+# In[5]:
 
 
 # Mobility can be one of the factors for increase in the AQI levels.
 # So one of the parameters can be density of railways and roadways.
 # Fetching Highways and Railways Data
 
-hr_gpd = gpd.read_file(WORKDIR + '/datasets/highways_railways.geojson')
-
-# In[9]:
+hr_gpd = gpd.read_file(WORKDIR+'/datasets/highways_railways.geojson')
 
 
-# Computing highway and railway line density
+# In[6]:
+
+
+#Computing highway and railway line density
 for i in range(0, len(locations)):
-    a1 = gdf.iloc[i]
+    a1 = gdf.iloc[i]    
     road_density = hr_gpd['geometry'].intersection(a1['geometry']).length
     print('Road Density Values')
-    print(a1['name'] + '=' + str(road_density.sum()))
+    print(a1['name']+'='+str(road_density.sum()))
 
-# In[10]:
+
+# In[7]:
 
 
 # Observations from the Road Density Values
@@ -108,26 +102,28 @@ for i in range(0, len(locations)):
 # Loni, Ghaziabad has the least highway and railway density in the region.
 
 
-# In[11]:
+# In[8]:
 
 
 # Forest cover is another important indicator for AQI levels.
 # Loading Forest Layer
 
-for_gpd = gpd.read_file(WORKDIR + '/datasets/forests.geojson')
+for_gpd = gpd.read_file(WORKDIR+'/datasets/forests.geojson')
 for_gpd['geometry'] = for_gpd['geometry'].buffer(0)
 
-# In[12]:
+
+# In[9]:
 
 
-# Computing Forest density
+#Computing Forest density
 for i in range(0, len(locations)):
     a1 = gdf.iloc[i]
     forest_density = for_gpd['geometry'].intersection(a1['geometry']).area
     print('Forest density')
-    print(a1['name'] + '=' + str(forest_density.sum()))
+    print(a1['name']+'='+str(forest_density.sum()))
 
-# In[13]:
+
+# In[10]:
 
 
 # Observations from the Forest Density Analysis
@@ -136,27 +132,29 @@ for i in range(0, len(locations)):
 # Loni Ghaziabad has the least forest density cover.
 
 
-# In[14]:
+# In[11]:
 
 
 # Industries are another important indicator for the AQI levels.
 # Density of industries is another parameter for the analysis.
 # Loading Industries Layer
 
-ind_gpd = gpd.read_file(WORKDIR + '/datasets/industrial.geojson')
+ind_gpd = gpd.read_file(WORKDIR+'/datasets/industrial.geojson')
 ind_gpd['geometry'] = ind_gpd['geometry'].buffer(0)
 
-# In[15]:
+
+# In[12]:
 
 
-# Computing Industrial density
+#Computing Industrial density
 for i in range(0, len(locations)):
     a1 = gdf.iloc[i]
     print('Industry density')
     ind_density = ind_gpd['geometry'].intersection(a1['geometry']).area
-    print(a1['name'] + '=' + str(ind_density.sum()))
+    print(a1['name']+'='+str(ind_density.sum()))
 
-# In[16]:
+
+# In[13]:
 
 
 # Observations from the industries layer
@@ -164,7 +162,7 @@ for i in range(0, len(locations)):
 # Mandir marg has highest density of industries among the other two regions.
 
 
-# In[17]:
+# In[1]:
 
 
 # Loading aerosol data and the distance arrays
@@ -174,37 +172,53 @@ import numpy as np
 
 dataset = pd.DataFrame()
 
-aerosolArray = np.array(gdal_array.LoadFile(WORKDIR + '/datasets/aerosol_layer.tif'))
-dataset['aerosol'] = np.ndarray.flatten(aerosolArray)
+aerosolArray = np.array(gdal_array.LoadFile(WORKDIR+'/datasets/aerosol_layer0401.tif'))
+dataset['aerosol01'] = np.ndarray.flatten(aerosolArray)
+cloudCover01 =  0.03 # Metadata: Land Cloud cover collected from the Landsat Website
 
-dist_forest = np.array(gdal_array.LoadFile(WORKDIR + '/datasets/dist_from_forests.tif'))
+aerosolArray = np.array(gdal_array.LoadFile(WORKDIR+'/datasets/aerosol_layer0424.tif'))
+dataset['aerosol24'] = np.ndarray.flatten(aerosolArray)
+cloudCover24 = 41.56 # Metadata: Land Cloud cover collected from the Landsat Website
+
+dist_forest = np.array(gdal_array.LoadFile(WORKDIR+'/datasets/dist_from_forests.tif'))
 dataset['dforest'] = np.ndarray.flatten(dist_forest)
 
-dist_highways = np.array(gdal_array.LoadFile(WORKDIR + '/datasets/dist_from_highways.tif'))
+dist_highways = np.array(gdal_array.LoadFile(WORKDIR+'/datasets/dist_from_highways.tif'))
 dataset['dhighways'] = np.ndarray.flatten(dist_highways)
 
-dist_industries = np.array(gdal_array.LoadFile(WORKDIR + '/datasets/dist_from_industries.tif'))
+dist_industries = np.array(gdal_array.LoadFile(WORKDIR+'/datasets/dist_from_industries.tif'))
 dataset['dindustries'] = np.ndarray.flatten(dist_industries)
 
-dataset = dataset[dataset['aerosol'] > aerosolArray.min()]
+dataset = dataset[dataset['aerosol01']>aerosolArray.min()]
+dataset = dataset[dataset['aerosol24']>aerosolArray.min()]
 
+# Comparing the aerosol levels
 # Finding Correlation between the proximity from different features and aerosol levels.
+print('\nAerosol Levels before lockdown')
+print('Mean aerosol level: ', dataset['aerosol01'].mean())
+print('Aerosol-Proximity from Forests: ',dataset['aerosol01'].corr(dataset['dforest'], method='pearson'))
+print('Aerosol-Proximity from Highways: ',dataset['aerosol01'].corr(dataset['dhighways'], method='pearson'))
+print('Aerosol-Proximity from Industries: ',dataset['aerosol01'].corr(dataset['dindustries'], method='pearson'))
+print('\nAerosol Levels after lockdown')
+print('Mean aerosol level: ', dataset['aerosol24'].mean())
+print('Aerosol-Proximity from Forests: ',dataset['aerosol24'].corr(dataset['dforest'], method='pearson'))
+print('Aerosol-Proximity from Highways: ',dataset['aerosol24'].corr(dataset['dhighways'], method='pearson'))
+print('Aerosol-Proximity from Industries: ',dataset['aerosol24'].corr(dataset['dindustries'], method='pearson'))
 
-print('Aerosol-Proximity from Forests: ', dataset['aerosol'].corr(dataset['dforest'], method='pearson'))
-print('Aerosol-Proximity from Highways: ', dataset['aerosol'].corr(dataset['dhighways'], method='pearson'))
-print('Aerosol-Proximity from Industries: ', dataset['aerosol'].corr(dataset['dindustries'], method='pearson'))
 
-# In[18]:
+# In[16]:
 
 
 # Observations
 
-# The pearson correlation between aerosol and distance from forests is the highest. This
-# indicates that there is a positive and stronger linear relationship between aerosol 
-# levels and proximity of forests than with industries and highways.
+# Mean aerosol levels are higher during the lockdown period.
+# The cloud cover during the period of lockdown is higher than during no lockdown. 
+# The pearson correlation between aerosol and proximity from forests is higher during no lockdown.
+# The pearson correlation between aerosol and proximity from highways is negative during no lockdown.
+# The pearson correlation between aerosol and proximity from highways is positive and higher during lockdown.
 
 
-# In[19]:
+# In[17]:
 
 
 # Summary of Observations from the analysis
@@ -221,9 +235,9 @@ print('Aerosol-Proximity from Industries: ', dataset['aerosol'].corr(dataset['di
 
 # Mandir marg has highest density of industries among the other two regions.
 
-# The pearson correlation between aerosol and distance from forests is the highest. This
-# indicates that there is a positive and stronger linear relationship between aerosol 
-# levels and proximity of forests than with industries and highways.
+# The pearson correlation between aerosol and proximity from forests is higher during no lockdown.
+# The pearson correlation between aerosol and proximity from highways is negative during no lockdown.
+# The pearson correlation between aerosol and proximity from highways is positive and higher during lockdown.
 
 # Conclusions
 
@@ -232,3 +246,7 @@ print('Aerosol-Proximity from Industries: ', dataset['aerosol'].corr(dataset['di
 
 
 # In[ ]:
+
+
+
+
